65af93dcbd28a3de9d499189480bcda3eb46f6c8
Walter Díaz <walddt05@gmail.com>
2022-03-12 14:08:02 -0600

    Domain, Infraestructure.

---
+++ Domain/Interfaces/IArchivotxtModel.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IArchivotxtModel:IModel<Archivotxt>
+    {
+        void EditText(Archivotxt t);
+        void EditName(Archivotxt t, string nn);
+    }
+}
---
+++ Infraestructure/Repositories/StreamArchivotxtRepository.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Repositories
+{
+    public class StreamArchivotxtRepository : IArchivotxtModel
+    {
+        string carpetaPath = Directory.GetCurrentDirectory() + @"\Archivos de Texto";
+        public void Create(Archivotxt t)
+        {
+            string archivoPath = @"\" + t.Nombre + ".txt";
+            File.Create(carpetaPath+ archivoPath);
+        }
+
+        public void Delete(Archivotxt t)
+        {
+            string archivoPath = @"\" + t.Nombre + ".txt";
+            File.Delete(carpetaPath + archivoPath);
+        }
+
+        public void EditName(Archivotxt t, string nn)
+        {
+            string nuevoNombrePath = @"\" + nn + ".txt";
+            string archivoPath = @"\" + t.Nombre + ".txt";
+            File.Move(carpetaPath + archivoPath, carpetaPath + nuevoNombrePath);
+        }
+
+        public void EditText(Archivotxt t)
+        {
+            string archivoPath = @"\" + t.Nombre + ".txt";
+            File.AppendAllText(carpetaPath + archivoPath, t.Contenido);
+        }
+    }
+}
