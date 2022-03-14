5136846b0f892ec00f6751c2dc06733ac92419d4
Walter Díaz <walddt05@gmail.com>
2022-03-13 22:06:10 -0600

    Agregar archivos de proyecto.

---
+++ Presentation/Bloc/Bloc_de_notas.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace Presentation.Bloc
+{
+    partial class Bloc_de_notas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Bloc_de_notas));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.trvArchivos = new System.Windows.Forms.TreeView();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStripEdit = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.arbirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cambiarNombreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevoToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnCambiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.contextMenuStripEdit.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.btnCambiar);
+            this.splitContainer1.Panel1.Controls.Add(this.btnCrear);
+            this.splitContainer1.Panel1.Controls.Add(this.txtNombre);
+            this.splitContainer1.Panel1.Controls.Add(this.trvArchivos);
+            this.splitContainer1.Panel1.Controls.Add(this.menuStrip1);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 266;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // trvArchivos
+            // 
+            this.trvArchivos.ContextMenuStrip = this.contextMenuStripEdit;
+            this.trvArchivos.Location = new System.Drawing.Point(12, 31);
+            this.trvArchivos.Name = "trvArchivos";
+            this.trvArchivos.Size = new System.Drawing.Size(251, 416);
+            this.trvArchivos.TabIndex = 1;
+            this.trvArchivos.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.trvArchivos_AfterSelect);
+            this.trvArchivos.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.trvArchivos_MouseDoubleClick);
+            this.trvArchivos.MouseUp += new System.Windows.Forms.MouseEventHandler(this.trvArchivos_MouseUp);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(515, 444);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // contextMenuStripEdit
+            // 
+            this.contextMenuStripEdit.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arbirToolStripMenuItem,
+            this.eliminarToolStripMenuItem,
+            this.cambiarNombreToolStripMenuItem,
+            this.nuevoToolStripMenuItem});
+            this.contextMenuStripEdit.Name = "contextMenuStripEdit";
+            this.contextMenuStripEdit.Size = new System.Drawing.Size(167, 92);
+            this.contextMenuStripEdit.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStripEdit_Opening);
+            // 
+            // arbirToolStripMenuItem
+            // 
+            this.arbirToolStripMenuItem.Name = "arbirToolStripMenuItem";
+            this.arbirToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.arbirToolStripMenuItem.Text = "Arbir";
+            this.arbirToolStripMenuItem.Click += new System.EventHandler(this.arbirToolStripMenuItem_Click);
+            // 
+            // eliminarToolStripMenuItem
+            // 
+            this.eliminarToolStripMenuItem.Name = "eliminarToolStripMenuItem";
+            this.eliminarToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.eliminarToolStripMenuItem.Text = "Eliminar";
+            this.eliminarToolStripMenuItem.Click += new System.EventHandler(this.eliminarToolStripMenuItem_Click);
+            // 
+            // cambiarNombreToolStripMenuItem
+            // 
+            this.cambiarNombreToolStripMenuItem.Name = "cambiarNombreToolStripMenuItem";
+            this.cambiarNombreToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.cambiarNombreToolStripMenuItem.Text = "Cambiar Nombre";
+            this.cambiarNombreToolStripMenuItem.Click += new System.EventHandler(this.cambiarNombreToolStripMenuItem_Click);
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            this.nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.nuevoToolStripMenuItem.Text = "Nuevo";
+            this.nuevoToolStripMenuItem.Click += new System.EventHandler(this.nuevoToolStripMenuItem_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(72, 3);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 3;
+            this.txtNombre.Visible = false;
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(178, 2);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(75, 23);
+            this.btnCrear.TabIndex = 4;
+            this.btnCrear.Text = "Crear";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Visible = false;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(266, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevoToolStripMenuItem1,
+            this.guardarToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // nuevoToolStripMenuItem1
+            // 
+            this.nuevoToolStripMenuItem1.Name = "nuevoToolStripMenuItem1";
+            this.nuevoToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.nuevoToolStripMenuItem1.Text = "Nuevo";
+            this.nuevoToolStripMenuItem1.Click += new System.EventHandler(this.nuevoToolStripMenuItem1_Click);
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.guardarToolStripMenuItem.Text = "Guardar y cerrar";
+            this.guardarToolStripMenuItem.Click += new System.EventHandler(this.guardarToolStripMenuItem_Click);
+            // 
+            // btnCambiar
+            // 
+            this.btnCambiar.Location = new System.Drawing.Point(179, 4);
+            this.btnCambiar.Name = "btnCambiar";
+            this.btnCambiar.Size = new System.Drawing.Size(75, 23);
+            this.btnCambiar.TabIndex = 6;
+            this.btnCambiar.Text = "Cambiar";
+            this.btnCambiar.UseVisualStyleBackColor = true;
+            this.btnCambiar.Visible = false;
+            this.btnCambiar.Click += new System.EventHandler(this.btnCambiar_Click);
+            // 
+            // Bloc_de_notas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Bloc_de_notas";
+            this.Text = "Bloc_de_notas";
+            this.Load += new System.EventHandler(this.Bloc_de_notas_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.contextMenuStripEdit.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView trvArchivos;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripEdit;
+        private System.Windows.Forms.ToolStripMenuItem arbirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eliminarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cambiarNombreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevoToolStripMenuItem;
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevoToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.Button btnCambiar;
+    }
+}
---
+++ Presentation/Bloc/Bloc_de_notas.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentation.Bloc
+{
+    public partial class Bloc_de_notas : Form
+    {
+        string carpeta = Application.StartupPath + @"\Archivos de texto";
+        public Bloc_de_notas()
+        {
+            try
+            {
+                if (!Directory.Exists(carpeta))
+                {
+                    MessageBox.Show("La carpeta no existe, creando");
+                    Directory.CreateDirectory(carpeta);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            InitializeComponent();
+        }
+        private TreeNode CrearArbol(DirectoryInfo directoryInfo)
+        {
+            TreeNode treeNode = new TreeNode(directoryInfo.Name);
+
+            foreach (var item in directoryInfo.GetDirectories())
+            {
+                treeNode.Nodes.Add(item.Name);
+            }
+            foreach (var item in directoryInfo.GetFiles())
+            {
+                treeNode.Nodes.Add(new TreeNode(item.Name));
+            }
+            return treeNode;
+        }
+
+
+        private void Bloc_de_notas_Load(object sender, EventArgs e)
+        {
+
+            trvArchivos.Nodes.Clear();
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(carpeta);
+
+            trvArchivos.Nodes.Add(CrearArbol(directoryInfo));
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void trvArchivos_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void nuevoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtNombre.Visible = true;
+            btnCrear.Visible = true;
+            richTextBox1.Clear();
+            
+        }
+
+        private void cambiarNombreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtNombre.Visible = true;
+            btnCambiar.Visible = true;
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            File.AppendAllText(carpeta + @"\" + txtNombre.Text + ".txt",richTextBox1.Text);
+            trvArchivos.Nodes.Clear();
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(carpeta);
+
+            trvArchivos.Nodes.Add(CrearArbol(directoryInfo));
+            
+            btnCrear.Visible = false;
+            txtNombre.Clear();
+            txtNombre.Visible = false;
+            richTextBox1.Clear();
+
+        }
+
+        private void trvArchivos_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            richTextBox1.Clear();
+            string ruta = trvArchivos.SelectedNode.FullPath;
+            string extension = Path.GetExtension(ruta);
+            if (extension == ".txt")
+            {
+                richTextBox1.Text = File.ReadAllText(ruta);
+            }
+            else
+            {
+                MessageBox.Show("No es un archivode texto");
+            }
+        }
+
+        private void eliminarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string path =trvArchivos.SelectedNode.FullPath;
+            trvArchivos.SelectedNode.Remove();
+            File.Delete(path);
+        }
+
+        private void arbirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            string ruta;
+            if (trvArchivos.SelectedNode == null)
+            {
+                ruta = carpeta;
+            }
+            else
+            {
+                ruta = trvArchivos.SelectedNode.FullPath;
+            }
+            
+            string extension = Path.GetExtension(ruta);
+            if (extension == ".txt")
+            {
+                richTextBox1.Text = File.ReadAllText(ruta);
+            }
+            else
+            {
+                MessageBox.Show("No es un archivode texto");
+            }
+        }
+
+        private void trvArchivos_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                string ruta;
+                string extension;
+                try
+                {
+                    trvArchivos.SelectedNode = trvArchivos.GetNodeAt(e.X, e.Y);
+                    
+                    if(trvArchivos.SelectedNode == null)
+                    {
+                        ruta = carpeta;
+                    }
+                    else
+                    {
+                        ruta = trvArchivos.SelectedNode.FullPath;
+                    }
+                    extension = Path.GetExtension(ruta);
+                    if (extension == ".txt")
+                    {
+                        nuevoToolStripMenuItem.Enabled = false;
+                        eliminarToolStripMenuItem.Enabled = true;
+                        cambiarNombreToolStripMenuItem.Enabled = true;
+                        arbirToolStripMenuItem.Enabled = true;
+                    }
+                    else
+                    {
+                        nuevoToolStripMenuItem.Enabled = true;
+                        eliminarToolStripMenuItem.Enabled = false;
+                        cambiarNombreToolStripMenuItem.Enabled = false;
+                        arbirToolStripMenuItem.Enabled = false;
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Error: " +ex.Message );
+                }
+              
+            }
+
+        }
+
+        private void contextMenuStripEdit_Opening(object sender, CancelEventArgs e)
+        {
+           
+        }
+
+        private void nuevoToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            txtNombre.Visible = true;
+            btnCrear.Visible = true;
+
+        }
+
+        private void btnCambiar_Click(object sender, EventArgs e)
+        {
+            string path = trvArchivos.SelectedNode.FullPath;
+            string newpath = carpeta + @"\" + txtNombre.Text + ".txt";
+            File.Move(path, newpath);
+            trvArchivos.Nodes.Clear();
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(carpeta);
+
+            trvArchivos.Nodes.Add(CrearArbol(directoryInfo));
+
+            btnCambiar.Visible = false;
+            txtNombre.Clear();
+            txtNombre.Visible = false;
+        }
+
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string path =trvArchivos.SelectedNode.FullPath;
+            File.AppendAllText(path, richTextBox1.Text);
+            trvArchivos.Nodes.Clear();
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(carpeta);
+
+            trvArchivos.Nodes.Add(CrearArbol(directoryInfo));
+            richTextBox1.Clear();
+        }
+    }
+}
